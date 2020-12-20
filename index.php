ede46323dba5da8f91d4d1ab2b50a97ef4faeac9
WrexrHD <76259140+WrexrHD@users.noreply.github.com>
2020-12-20 13:22:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,13 @@
   include_once ('header.php');
   include_once ('footer.php');
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
